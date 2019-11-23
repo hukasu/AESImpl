ad1637fd5722f49e873e6c9eb2644e749ac51a61
Lucas Farias <lucas_fnf@hotmail.com>
2019-11-23 14:14:15 -0300

    Add method and type declarations

--- src/aes_lib.cpp
+++ src/aes_lib.cpp
@@ -1,5 +1,27 @@
 #include "aes_lib.hpp"
 
 namespace aes {
+	BlockType encrypt(BlockType block, Key128Type key) {
+		return BlockType{};
+	}
 
+	BlockType encrypt(BlockType block, Key192Type key) {
+		return BlockType{};
+	}
+
+	BlockType encrypt(BlockType block, Key256Type key) {
+		return BlockType{};
+	}
+
+	BlockType decrypt(BlockType block, Key128Type key) {
+		return BlockType{};
+	}
+
+	BlockType decrypt(BlockType block, Key192Type key) {
+		return BlockType{};
+	}
+
+	BlockType decrypt(BlockType block, Key256Type key) {
+		return BlockType{};
+	}
 }
--- src/aes_lib.hpp
+++ src/aes_lib.hpp
@@ -1,8 +1,24 @@
 #ifndef __AES__LIB__HPP__
 #define __AES__LIB__HPP__
 
-namespace aes {
+#include <cstdlib>
+#include <string>
+#include <array>
 
+typedef std::array<uint8_t, 128> BlockType;
+
+typedef std::array<uint8_t, 128> Key128Type;
+typedef std::array<uint8_t, 192> Key192Type;
+typedef std::array<uint8_t, 256> Key256Type;
+
+namespace aes {
+	BlockType encrypt(BlockType block, Key128Type key);
+	BlockType encrypt(BlockType block, Key192Type key);
+	BlockType encrypt(BlockType block, Key256Type key);
+
+	BlockType decrypt(BlockType block, Key128Type key);
+	BlockType decrypt(BlockType block, Key192Type key);
+	BlockType decrypt(BlockType block, Key256Type key);
 }
 
 #endif // __AES__LIB__HPP__
